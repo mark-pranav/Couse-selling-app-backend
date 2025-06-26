bd18f307ec74c8e3c64384bdaa35d247f70e87d9
Pranav Bobade <pranavbobade48@gmail.com>
2025-06-26 21:02:03 +0530

    CORE LOGIC OF AUTHENTICATION WRITTEN

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,102 @@
 const {Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db")
+const {z} = require('zod');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const JWT_ADMIN_SECRET = "IamBoss15422";
 
 
 
 
-adminRouter.post("/signup" , function(req,res){
-    res.json({
-        message: "You're signed up"
-    })
+adminRouter.post("/signup" , async function(req,res){
+   const requireBody = z.object({
+        email : z.string().min(5).max(50).email(),
+        firstName : z.string().min(3).max(50),
+        lastName: z.string().min(3).max(20),
+        password : z.string().min(6).max(20).refine(
+            (password) => /[A-Z]/.test(password),
+            {
+                message: "Password must contain atleast on UpperCase character"
+            }
+        ).refine(
+            (password) => /[a-z]/.test(password),
+            {
+                message: "Password must contain atleast one Lowercase character"
+            }
+        )
+    });
+
+    const parseDatawithSuccess = requireBody.safeParse(req.body); 
+
+    if(!parseDatawithSuccess.success){
+        res.json({
+            message: "failed ",
+            error : parseDatawithSuccess.error
+            
+        })
+        return
+    }
+
+    // ended
+    // authentication using bcrypt started
+    const { email , firstName , lastName , password } = req.body;
+    let errorFlag = false;
+
+    try{ 
+        const hashPassword = await bcrypt.hash(password, 5);
+        console.log(hashPassword);
+
+        await adminModel.create({
+            email : email,
+            firstName : firstName ,
+            lastName : lastName, 
+            password: hashPassword
+        })
+
+    }catch(e){
+        res.json({
+            message: "User already exists"
+        })
+        errorFlag=true;
+    }
+
+    if(!errorFlag){
+
+        res.json({
+            message: "you are logged in"
+        })
+    }
 })
 
-adminRouter.post("/signin" , function(req,res){
-    res.json({
-        message: "You're signed up"
-    })
+adminRouter.post("/signin" , async function(req,res){
+    const { email , password } = req.body;
+
+    const admin = await userModel.findOne({
+        email : email
+    });
+    if(!admin){
+        res.status(403).json({
+            message: "Account doesn't exist , Signup first"
+        })
+    }
+
+
+    const matchPassword = await bcrypt.compare(password, admin.password);
+    if(matchPassword){
+        const token = jwt.sign({
+            id : admin._id.toString()
+        }, JWT_ADMIN_SECRET);
+        res.json({
+            token
+        })
+    }
+    else{
+        res.status(403).json({
+        message: "Incorrect credentials"
+        });
+    }
 })
 
 adminRouter.put("/createCourse" , function(req,res){
--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,111 @@
-const {Router } = require("express");
+const { Router } = require("express");
 const userRouter = Router();
 const { userModel } = require("../db");
+const z = require("zod");
+const  bcrypt  = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const JWT_USER_SECRET = "PRANAV252002";
+
+userRouter.post("/signup", async function (req, res) {
+    // form field validation using zod 
+    const requireBody = z.object({
+        email : z.string().min(5).max(50).email(),
+        firstName : z.string().min(3).max(50),
+        lastName: z.string().min(3).max(20),
+        password : z.string().min(6).max(20).refine(
+            (password) => /[A-Z]/.test(password),
+            {
+                message: "Password must contain atleast on UpperCase character"
+            }
+        ).refine(
+            (password) => /[a-z]/.test(password),
+            {
+                message: "Password must contain atleast one Lowercase character"
+            }
+        )
+    });
+
+    const parseDatawithSuccess = requireBody.safeParse(req.body); 
+
+    if(!parseDatawithSuccess.success){
+        res.json({
+            message: "failed ",
+            error : parseDatawithSuccess.error
+            
+        })
+        return
+    }
+
+    // ended
+    // authentication using bcrypt started
+    const { email , firstName , lastName , password } = req.body;
+    let errorFlag = false;
+
+    try{ 
+        const hashPassword = await bcrypt.hash(password, 5);
+        console.log(hashPassword);
+
+        await userModel.create({
+            email : email,
+            firstName : firstName ,
+            lastName : lastName, 
+            password: hashPassword
+        })
+
+    }catch(e){
+        res.json({
+            message: "User already exists"
+        })
+        errorFlag=true;
+    }
+
+    if(!errorFlag){
+
+        res.json({
+            message: "you are logged in"
+        })
+    }
+
+});
+
+userRouter.post("/signin", async function (req, res) {
+    const { email , password } = req.body;
+
+    const user = await userModel.findOne({
+        email : email
+    });
+    if(!user){
+        res.status(403).json({
+            message: "Account doesn't exist , Signup first"
+        })
+    }
 
 
+    const matchPassword = await bcrypt.compare(password, user.password);
+    if(matchPassword){
+        const token = jwt.sign({
+            id : user._id.toString()
+        }, JWT_USER_SECRET);
+        res.json({
+            token
+        })
+    }
+    else{
+        res.status(403).json({
+        message: "Incorrect credentials"
+        });
+    }
 
 
-userRouter.post("/signup" , function(req,res){
-    const {email , password, firstName, lastName} = req.body;
-    //this can be done by const email = req.body.email and so on , we're just destructured the element  
+});
 
-    res.json({
-        message: "You're signed up"
-    })
-})
-
-
-
-userRouter.post("/signin" , function(req,res){
-    res.json({
-        message: "You're signed up"
-    })
-})
-
-
-
-
-userRouter.get("/purchases" , function(req,res){
-    res.json({
-        message: "You're purchases course"
-    })
-})
-
+userRouter.get("/purchases", function (req, res) {
+  res.json({
+    message: "You're purchases course",
+  });
+});
 
 module.exports = {
-    userRouter:userRouter
-}
+  userRouter: userRouter,
+};
